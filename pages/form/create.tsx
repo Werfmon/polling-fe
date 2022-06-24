3e11e5010defa714b9ae0cf117361fdec57237ae
Werfmon <d.vyroubal.w@gmail.com>
2022-06-24 22:21:48 +0200

    question adding

--- pages/form/create.tsx
+++ pages/form/create.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import type { NextPage } from 'next'
-import styled from 'styled-components'
-
+import React, { useState } from "react";
+import type { NextPage } from "next";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
 const Container = styled.div`
- background-color: #000;
- min-height: 100vh;
- padding-block: 30px;
- h1 {
-   width: 70%;
-   margin: 0 auto;
-   color: #fff;
-   font-weight: 100;
-   height: 4rem;
- }
-`
+  background-color: #000;
+  min-height: 100vh;
+  padding-block: 30px;
+  h1 {
+    width: 70%;
+    margin: 0 auto;
+    color: #fff;
+    font-weight: 100;
+    height: 4rem;
+  }
+`;
 const FormContainer = styled.div`
   width: 70%;
   min-height: 100vh;
@@ -23,14 +24,12 @@ const FormContainer = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-`
+`;
 const Form = styled.form`
   height: 100%;
   width: 100%;
-`
-const Input = styled.input`
-
-`
+`;
+const Input = styled.input``;
 const FormInputName = styled.input`
   background-color: unset;
   width: 70%;
@@ -44,8 +43,7 @@ const FormInputName = styled.input`
     text-align: center;
     font-size: 3rem;
   }
-
-`
+`;
 const FormInputDescription = styled.input`
   background-color: unset;
   width: 70%;
@@ -59,8 +57,7 @@ const FormInputDescription = styled.input`
     text-align: center;
     font-size: 2rem;
   }
-
-`
+`;
 const QuestionInput = styled.input`
   background-color: unset;
   margin-top: 30px;
@@ -70,8 +67,7 @@ const QuestionInput = styled.input`
   &::placeholder {
     font-size: 2rem;
   }
-
-`
+`;
 const AnswerInput = styled.input`
   background-color: unset;
   margin-top: 30px;
@@ -82,118 +78,112 @@ const AnswerInput = styled.input`
   &::placeholder {
     font-size: 2rem;
   }
-`
+`;
 const QuestionContainer = styled.div`
   width: 70%;
   margin: 0 auto;
   margin-top: 30px;
   font-size: 2rem;
   padding: 10px;
-`
+`;
 const AnswerContainer = styled.div`
   padding: 10px;
   margin-left: 30px;
   display: flex;
   flex-direction: column;
-`
+`;
 const InputContainer = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-`
+`;
 const CorrectRadio = styled.input`
-display: flex;
-margin-top: 10px;
-`
+  display: flex;
+  margin-top: 10px;
+`;
 const CorrectRadioContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 4rem;
+`;
+const AddQuestionbutton = styled.button`
+  color: #fff;
+  background-color: unset;
+  width: 100%;
+  cursor: pointer;
+  height: 6rem;
+`;
+const CreateButton = styled.button`
+  color: #fff;
+  background-color: unset;
+  height: 6rem;
+  font-size: 2rem;
+font-weight: 100;
+`
+const CreateButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  width: 90%;
+
 `
 const Create: NextPage = () => {
+  const [questions, setQuestions] = useState([null]);
+  function addQuestion(e: any) {
+    e.preventDefault();
+    setQuestions([...questions, null]);
+  }
+
   return (
     <Container>
       <h1>Create Form</h1>
       <FormContainer>
         <Form>
           <InputContainer>
-            <FormInputName type='text' name='formName' placeholder='Form name'/>
+            <FormInputName
+              type="text"
+              name="formName"
+              placeholder="Form name"
+            />
           </InputContainer>
           <InputContainer>
-            <FormInputDescription maxLength={100} type='text' name='formDescription' placeholder='Form description'/>
+            <FormInputDescription
+              maxLength={100}
+              type="text"
+              name="formDescription"
+              placeholder="Form description"
+            />
           </InputContainer>
-          <QuestionContainer>
-            <QuestionInput placeholder='Question...'/>
-            <AnswerContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-            </AnswerContainer>
-          </QuestionContainer>
-          <QuestionContainer>
-            <QuestionInput placeholder='Question...'/>
-            <AnswerContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-            </AnswerContainer>
-          </QuestionContainer>
-          <QuestionContainer>
-            <QuestionInput placeholder='Question...'/>
-            <AnswerContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-            </AnswerContainer>
-          </QuestionContainer>
-          <QuestionContainer>
-            <QuestionInput placeholder='Question...'/>
-            <AnswerContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-              <CorrectRadioContainer>
-                <AnswerInput placeholder='Answer...'/>
-                <CorrectRadio name='correct' type='radio' />
-              </CorrectRadioContainer>
-            </AnswerContainer>
-          </QuestionContainer>
+          {questions.map((_, i) => (
+            <QuestionContainer className='questionContainer'>
+              <QuestionInput placeholder="Question..." />
+                <AnswerContainer>
+                  <CorrectRadioContainer>
+                    <AnswerInput placeholder="Answer..." />
+                    <CorrectRadio name={"correct" + i} type="radio" />
+                  </CorrectRadioContainer>
+                  <CorrectRadioContainer>
+                    <AnswerInput placeholder="Answer..." />
+                    <CorrectRadio name={"correct" + i} type="radio" />
+                  </CorrectRadioContainer>
+                  <CorrectRadioContainer>
+                    <AnswerInput placeholder="Answer..." />
+                    <CorrectRadio name={"correct" + i} type="radio" />
+                  </CorrectRadioContainer>
+                </AnswerContainer>
+              </QuestionContainer>
+            )) 
+          }
+          <AddQuestionbutton>
+            <FontAwesomeIcon onClick={addQuestion} icon={faAdd} size="2x" />
+          </AddQuestionbutton>
+          <CreateButtonContainer>
+            <CreateButton>Create</CreateButton>
+          </CreateButtonContainer>
         </Form>
       </FormContainer>
     </Container>
-  )
-}
+  );
+};
 export default Create;
